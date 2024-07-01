96bcb7372c012d672e9215061fc1e335a28b7d9b
BRINDEL Leonard <leonard.brindel@inria.fr>
2024-07-01 15:48:13 +0200

    Removed non-relevant test dataframe in barplot case or empty dictionnary.

--- m2m_postaviz/shiny_app.py
+++ m2m_postaviz/shiny_app.py
@@ -234,11 +234,20 @@ def run_shiny(data: DataStorage):
                         tested_data[layer_1] = {}
                         for layer_2 in df[x2].unique():
                             selected_data = df.loc[(df[x1] == layer_1) & (df[x2] == layer_2)][column_value]
-                            if len(selected_data) != 0:
+                            if len(selected_data) > 1:
                                 tested_data[layer_1][layer_2] = {}
                                 tested_data[layer_1][layer_2]["data"] = selected_data.values
                                 tested_data[layer_1][layer_2]["n_data"] = len(selected_data)
-                    res = du.stat_on_plot(tested_data, 2)
+                    # In case tested_data has no value (barplot)
+                    if all(len(tested_data[k]) == 0 for k in tested_data.keys()):
+                        print("No values in each keys")
+                        return
+                    # In case tested_data is not a double layer dict
+                    if all(len(tested_data[k]) == 1 for k in tested_data.keys()):
+                        print("Only one pair per layer.")
+                        return
+                    else:
+                        res = du.stat_on_plot(tested_data, 2)
                     all_dataframe["producer_test_df"] = res
                     return res
         
@@ -247,7 +256,6 @@ def run_shiny(data: DataStorage):
         def save_file():
             if bool(all_dataframe):
                 if not all_dataframe["producer_test_df"] is None:
-                    res = production_test_dataframe()
                     data.save_dataframe(all_dataframe["producer_test_df"], "producer_test_dataframe")
             return
         
@@ -256,7 +264,6 @@ def run_shiny(data: DataStorage):
         def save_file():
             if bool(all_dataframe):
                 if all_dataframe["abundance_test_df"] is not None:
-                    res = production_test_dataframe()
                     data.save_dataframe(all_dataframe["abundance_test_df"], "abundance_test_dataframe")
             return
         
@@ -265,7 +272,6 @@ def run_shiny(data: DataStorage):
         def save_file():
             if bool(all_dataframe):
                 if all_dataframe["abundance_plot_df"] is not None:
-                    res = production_test_dataframe()
                     data.save_dataframe(all_dataframe["abundance_plot_df"], "abundance_plot_dataframe")
             return
         
@@ -274,7 +280,6 @@ def run_shiny(data: DataStorage):
         def save_file():
             if bool(all_dataframe):
                 if all_dataframe["producer_plot_df"] is not None:
-                    res = production_test_dataframe()
                     data.save_dataframe(all_dataframe["producer_plot_df"], "producer_plot_dataframe")
             return
 
