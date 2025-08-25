aaa37679acee4ff3492975460fc8c8a0e1ea398e
Clémence Frioux <clemence.frioux@inria.fr>
2025-08-25 09:54:56 +0200

    Bump version: 0.1.0 → 0.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ project = "m2m-postaviz"
 year = "2023"
 author = "Léonard Brindel, Clémence Frioux"
 copyright = f"{year}, {author}"
-version = release = "0.1.0"
+version = release = "0.1.1"
 
 pygments_style = "trac"
 templates_path = ["."]
